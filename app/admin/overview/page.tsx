0e659118c1f5d77391aa01e02030477c83e41895
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2025-09-25 10:13:16 +0300

    Order code cleanup

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,12 +1,13 @@
 import { auth } from '@/auth'
-import OverviewReport from './overview-report'
+// import OverviewReport from './overview-report'
 
 const DashboardPage = async () => {
   const session = await auth()
   if (session?.user.role !== 'Admin')
     throw new Error('Admin permission required')
 
-  return <OverviewReport />
+  // return <OverviewReport />
+  return <div>OverviewReport</div>
 }
 
 export default DashboardPage
